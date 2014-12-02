31be6d7e69966a9d3741dbe9ead48e48e77769a6
amoratka <kamilaciok@wp.pl>
2014-12-02 16:12:02 +0100

    Podział na kilka klas

---
+++ src/tsp/Package.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsp;
+
+/**
+ *
+ * @author Kamila
+ */
+public class Package {
+    int packageID;
+    String name;
+}
